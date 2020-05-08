7b7579ddd37c4f11b3fb8c5df078e7a415a31241
marcywebdev <marcelinojasso@hotmail.com>
2020-05-07 21:41:57 -0700

    add array for guessing game characters

---
+++ js/main.js
@@ -0,0 +1,1 @@
+const pics = ["alex.png", "anita.png", "peter.png", "eric.png", "charles.png", "sam.png", "joe.png", "maria.png", "philip.png", "richard.png", "susan.png", "max.png", "alfred.png", "robert.png", "frans.png", "claire.png", "paul.png", "bill.png", "david.png", "bernard.png", "george.png", "tom.png", "herman.png", "anne.png"];
