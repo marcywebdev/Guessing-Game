a7c581023f0b54230ac5f161f6e83ec5dc25c62d
marcywebdev <marcelinojasso@hotmail.com>
2020-05-09 19:26:26 -0700

    add back.png to images. add jquery function on flipping card animation. add css style.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 const pics = ["alex.png", "anita.png", "peter.png", "eric.png", "charles.png", "sam.png", "joe.png", "maria.png", "philip.png", "richard.png", "susan.png", "max.png", "alfred.png", "robert.png", "frans.png", "claire.png", "paul.png", "bill.png", "david.png", "bernard.png", "george.png", "tom.png", "herman.png", "anne.png"];
+
+$('.card').click(function () {
+  if ($(this).hasClass('out'))
+    $(this).removeClass('out').addClass('in');
+  else
+    $(this).addClass('out').removeClass('in');
+});
