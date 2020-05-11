1bbb5314cbba4ce083822166de1438d278cec6aa
marcywebdev <marcelinojasso@hotmail.com>
2020-05-10 21:30:13 -0700

    add js function on picking random pic for the player with name. add media query to test on mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 const pics = ["alex.png", "anita.png", "peter.png", "eric.png", "charles.png", "sam.png", "joe.png", "maria.png", "philip.png", "richard.png", "susan.png", "max.png", "alfred.png", "robert.png", "frans.png", "claire.png", "paul.png", "bill.png", "david.png", "bernard.png", "george.png", "tom.png", "herman.png", "anne.png"];
 
+function myCharacter() {
+  var myPic = Math.floor(Math.random() * pics.length);
+
+  $("#me").html('<img src="images/' + pics[myPic] + ' ">');
+
+  var name = pics[myPic].substr(0, pics[myPic].lastIndexOf("."))
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $("#name").html('You are ' + name);
+}
+
+myCharacter();
+
 $('.card').click(function () {
   if ($(this).hasClass('out'))
     $(this).removeClass('out').addClass('in');
